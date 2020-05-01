d3c627781258f986ca416622baac4b1a4ed9565d
Dev Daksan <dev.91096@gmail.com>
2020-05-01 19:02:04 +0530

    Added styles to error page

--- src/components/Error/index.js
+++ src/components/Error/index.js
@@ -2,8 +2,10 @@ import React from "react";
 
 function Error() {
   return (
-    <div>
-      <h1>Error</h1>
+    <div className="error">
+      <h1>Error while parsing your JSON input</h1>
+      <h2>Please make sure that the input format is correct and try again</h2>
+      <button onClick={() => window.location.reload()}>Try Again</button>
     </div>
   );
 }
