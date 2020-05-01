afcc231e23d5c021288c21388bb23ef5807becca
Dev Daksan <dev.91096@gmail.com>
2020-05-01 15:50:11 +0530

    Added parse for Date, Radio and Color

--- src/components/Form/parse.js
+++ src/components/Form/parse.js
@@ -14,7 +14,7 @@ const ParseElement = ({ key, value }) => {
     return (
       <div className={styles.item}>
         <label htmlFor={key}>{key}</label>
-        <select name={key}>
+        <select name={key} required>
           {value.map((option) => (
             <option key={option} value={option}>
               {option}
@@ -28,7 +28,38 @@ const ParseElement = ({ key, value }) => {
     return (
       <div className={styles.item}>
         <label htmlFor={key}>{key}</label>
-        <input type="number" name={key} />
+        <input type="number" name={key} required />
+      </div>
+    );
+  } else if (value.match(/yyyy|mm|dd/g)) {
+    // Check for Date
+    return (
+      <div className={styles.item}>
+        <label htmlFor={key}>{key}</label>
+        <input type="date" name={key} required />
+      </div>
+    );
+  } else if (value.split(/\//g).length > 1) {
+    // Check for Radio
+    let options = value.split(/\//g);
+
+    return (
+      <div className={styles.item}>
+        <label htmlFor={key}>{key}</label>
+        {options.map((option) => (
+          <div className={styles.radio} key={option}>
+            <input type="radio" name={key} id={option} />
+            <label htmlFor={option}>{option}</label>
+          </div>
+        ))}
+      </div>
+    );
+  } else if (value.match(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/g)) {
+    // Check for Color
+    return (
+      <div className={styles.item}>
+        <label htmlFor={key}>{key}</label>
+        <input type="color" name={key} />
       </div>
     );
   }
