1c1a71e85d097e6dfec10b629857adbb3c71a987
Dev Daksan <dev.91096@gmail.com>
2020-05-01 18:53:58 +0530

    Added Result Page

--- src/components/Form/result.js
+++ src/components/Form/result.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import styles from "./form.module.scss";
 
 function Result({ json }) {
+  let [result, setResult] = useState([]);
+
+  useEffect(() => {
+    let result = [];
+    for (let key in json) {
+      result.push({
+        key,
+        value: json[key],
+      });
+    }
+
+    setResult(result);
+  }, [json]);
+
   return (
-    <div className={styles.result}>
-      <pre>{JSON.stringify(json, null, 2)}</pre>
+    <div className={styles.results}>
+      <h1>Result From Your Submitted Form</h1>
+      {result.map((r) => (
+        <div className={styles.result} key={r.key}>
+          <h1>{r.key}</h1>
+          <h2>{r.value}</h2>
+        </div>
+      ))}
     </div>
   );
 }
